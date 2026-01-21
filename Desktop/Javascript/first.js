63d602e6e6d8f3473741e65ba47001368126d862
Prasad-alt779 <23x01a0569@nrcmec.org>
2026-01-21 14:49:51 +0530

    file added

---
+++ Desktop/Javascript/first.js
@@ -0,0 +1,7 @@
+const shirt = {
+    title: "cotton shirt",
+    rating: 3,
+    offer: 3,
+    price: 350
+};
+console.log(shirt);
---
+++ Desktop/Javascript/main.py
@@ -0,0 +1,25 @@
+from  tkinter import *
+from tkinter import ttk
+win = Tk()
+win.title("Hari Telco")
+win.config(bg="blue")
+win.geometry("500x500")
+
+name_label=Label(win,text="Hari Weather APP",font=("Time New Roman",30,"bold"))
+name_label.place(x=25,y=50,height=50,width=450)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+win.mainloop()
